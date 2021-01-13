cab01d76777b54c4ceb1f3fafa6f7d118958b3f2
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-13 17:57:27 +0530

    Learned Values & Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+let js = "Amazing";
+//if (js === "Amazing") alert("javaScript is FUN!");
+40 + 10 + 23 - 22;
+console.log("result===>", 40 + 10 + 23 - 22);
+
+// Values & Variable
+
+let firstName = "Khwaja";
+console.log("this is first name===>", firstName);
+console.log("this is first name===>", firstName);
+console.log("this is first name===>", firstName);
+
+let Khwaja_pasha = "KP";
+console.log(Khwaja_pasha);
+
+let job1 = "Marketing";
+let job2 = "Programmer";
+console.log("my first job--->", job1, "+", "my senond job--->", job2);
