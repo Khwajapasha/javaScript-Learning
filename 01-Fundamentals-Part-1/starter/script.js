bfe8e87a07a8082c1f1b791afd9bcbcc637487e5
khwajapasha141@gmail.com <71385280+Khwajapasha@users.noreply.github.com>
2021-01-13 19:27:35 +0530

    Learned Primitive data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,30 +1,55 @@
-let js = "Amazing";
-//if (js === "Amazing") alert("javaScript is FUN!");
-40 + 10 + 23 - 22;
-console.log("result===>", 40 + 10 + 23 - 22);
+// let js = "Amazing";
+// //if (js === "Amazing") alert("javaScript is FUN!");
+// 40 + 10 + 23 - 22;
+// console.log("result===>", 40 + 10 + 23 - 22);
 
-// Values & Variable
+// // Values & Variable
 
-let firstName = "Khwaja";
-console.log("this is first name===>", firstName);
-console.log("this is first name===>", firstName);
-console.log("this is first name===>", firstName);
+// let firstName = "Khwaja";
+// console.log("this is first name===>", firstName);
+// console.log("this is first name===>", firstName);
+// console.log("this is first name===>", firstName);
 
-let Khwaja_pasha = "KP";
-console.log(Khwaja_pasha);
+// let Khwaja_pasha = "KP";
+// console.log(Khwaja_pasha);
 
-let job1 = "Marketing";
-let job2 = "Programmer";
-console.log("my first job--->", job1, "+", "my senond job--->", job2);
+// let job1 = "Marketing";
+// let job2 = "Programmer";
+// console.log("my first job--->", job1, "+", "my senond job--->", job2);
 
-// Assignment:-->
-// 1. Declare variables called 'country', 'continent' and 'population' and
-// assign their values according to your own country (population in millions)
-// 2. Log their values to the console
+// // Assignment:-->
+// // 1. Declare variables called 'country', 'continent' and 'population' and
+// // assign their values according to your own country (population in millions)
+// // 2. Log their values to the console
 
-let country = "India";
-let continent = "Asia";
-let population = "135cr";
-console.log("my country--->", country);
-console.log("my continent--->", continent);
-console.log("my population--->", population);
+// let country = "India";
+// let continent = "Asia";
+// let population = 135;
+// console.log("my country--->", country);
+// console.log("my continent--->", continent);
+// console.log("my population--->", population);
+
+// ====== Data types =====
+// 1) Primative
+// 2) Object
+
+// 1) Primative:-->
+
+let lastName = "Khwaja"; // String
+console.log(typeof lastName);
+
+let age = 28; // Number
+console.log(typeof age);
+
+let javaScriptIsFun = true; // Boolean
+console.log(typeof javaScriptIsFun);
+
+// Reassining Value to Variable javaScriptIsFun
+javaScriptIsFun = "YES";
+console.log(typeof javaScriptIsFun);
+
+let year; // undefined
+console.log(typeof year);
+
+let month = null;
+console.log("this is null --->", month);
